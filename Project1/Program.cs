372cb2f01674f22209c93fecc271ccd8eabc8fc0
Noah Hoffman <stacknoah99@gmail.com>
2024-03-01 00:39:53 -0700

    Setting up basic logic for win cases

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -16,7 +16,43 @@ namespace Project1
             string userChoice = Console.ReadLine();
             Console.WriteLine("Your choice was: " + userChoice);
 
+            //Create a random number generator to decide the computers choice
+            Random randomChoice = new Random();
+            int computersChoice = randomChoice.Next(1, 4);
 
+            if (computersChoice == 1)
+            {
+                Console.WriteLine("The Computer picked Rock");
+            }
+
+            else if (computersChoice == 2)
+            {
+                Console.WriteLine("The Computer picked Paper");
+            }
+
+            else
+            {
+                Console.WriteLine("The Computer picked Scissors");
+            };
+
+
+            //Easy way to check if the user won. This is extremely inefficient due to all the if/else statements needed
+            if (computersChoice == 1 && userChoice == "Paper")
+            {
+                Console.WriteLine("You Won!");
+            }
+            else if (computersChoice == 2 && userChoice == "Scissors")
+            {
+                Console.WriteLine("You Won!");
+            }
+            else if (computersChoice == 3 && userChoice == "Rock")
+            {
+                Console.WriteLine("You Won!");
+            }
+            else
+            {
+                Console.WriteLine("You lost or there was a tie");
+            }
             Console.ReadLine();
         }
     }
