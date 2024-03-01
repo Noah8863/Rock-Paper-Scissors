969b8cce3677e08da875b6d22048ed034d6ddd40
White Yeti <White Yeti@WHITE-YETI>
2024-03-01 00:23:30 -0700

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my little game!");
+            Console.Write("Plese select either Rock, Paper or Scissors: ");
+            //Grab the users choice
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("Your choice was: " + userChoice);
+
+
+            Console.ReadLine();
+        }
+    }
+}
