3c387feae97b168849caa944be2c1552137fa5af
Noah Hoffman <stacknoah99@gmail.com>
2024-03-03 20:24:05 -0700

    Fixed some of the win/lost logic and added an option to play again

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -8,7 +8,28 @@ namespace Project1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            //Default behavior is to keep playing
+            bool playAgain = true;
+
+            //While true, play the game and show a question afterwards
+            while(playAgain)
+            {
+                PlayGame();
+
+                //Ask the user if they want to play again
+                Console.WriteLine("Want to play again? Y/N");
+                string userInput = Console.ReadLine();
+
+                //Making sure there isn't an error based on case sensitive input
+                playAgain = userInput.ToLower() == "y";
+                //If the user enters "y", it comes back as a 'true' value, thus playing the game again
+                //If the user enters anything but "y", the value turns 'false' and doesn't fire the play game function
+            }
+            Console.WriteLine("Thanks for playing!");
+        }
+        static void PlayGame()
         {
             Console.WriteLine("Welcome to my little game!");
             Console.Write("Plese select either Rock, Paper or Scissors: ");
@@ -20,38 +41,18 @@ namespace Project1
             Random randomChoice = new Random();
             int computersChoice = randomChoice.Next(1, 4);
 
-            if (computersChoice == 1)
-            {
-                Console.WriteLine("The Computer picked Rock");
-            }
-
-            else if (computersChoice == 2)
-            {
-                Console.WriteLine("The Computer picked Paper");
-            }
-
-            else
-            {
-                Console.WriteLine("The Computer picked Scissors");
-            };
-
-
             //Easy way to check if the user won. This is extremely inefficient due to all the if/else statements needed
-            if (computersChoice == 1 && userChoice == "Paper")
+            if (computersChoice == 1 && userChoice == "Paper" || computersChoice == 2 && userChoice == "Scissors" || computersChoice == 3 && userChoice == "Rock")
             {
                 Console.WriteLine("You Won!");
             }
-            else if (computersChoice == 2 && userChoice == "Scissors")
+            else if (computersChoice == 1 && userChoice == "Scissors" || computersChoice == 2 && userChoice == "Rock" || computersChoice == 3 && userChoice == "Paper")
             {
-                Console.WriteLine("You Won!");
-            }
-            else if (computersChoice == 3 && userChoice == "Rock")
-            {
-                Console.WriteLine("You Won!");
+                Console.WriteLine("The Computer Won");
             }
             else
             {
-                Console.WriteLine("You lost or there was a tie");
+                Console.WriteLine("Tie Game!");
             }
             Console.ReadLine();
         }
